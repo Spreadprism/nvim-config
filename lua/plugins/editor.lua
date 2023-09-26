c98b127bc76ef4dc7497d9fa8df1c43461d8e076
Emeric Duguay <emeric.duguay1@bell.ca>
2023-09-26 15:13:31 -0400

    Added markdown support

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -178,5 +178,18 @@ return {
       -- add any options here
     },
     lazy = false,
+  },
+  {
+     "iamcco/markdown-preview.nvim",
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+		config = function()
+			-- vim.cmd [[
+			-- 	function OpenMarkdownPreview (url)
+			-- 		execute "xdg-open " . a:url
+			-- 	endfunction
+			-- 	let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+			-- ]]
+		end, 
   }
 }
