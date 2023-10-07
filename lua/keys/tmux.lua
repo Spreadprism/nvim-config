f8201ef466eec4a0a83dd1b3dfd57e2264d9845b
Emeric Duguay <emericduguay@gmail.com>
2023-10-06 21:02:29 -0400

    small move

--- lua/keys/tmux.lua
+++ lua/keys/tmux.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.n = {
-  -- Tmux navigation
   ["<C-w>"] = {"<cmd>TmuxNavigateLeft<CR>", "Navigate window left", { noremap = true }},
   ["<C-l>"] = {"<cmd>TmuxNavigateRight<CR>", "Navigate window right", { noremap = true }},
   ["<C-j>"] = {"<cmd>TmuxNavigateDown<CR>", "Navigate window down", { noremap = true }},
