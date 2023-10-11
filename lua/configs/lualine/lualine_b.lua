4c8039ed1709ddd91701b26976f140b769ff6cfb
Emeric Duguay <emeric.duguay1@bell.ca>
2023-10-11 17:25:56 -0400

    Modified the lualine filetype display condition
    Added the concept of a buffername blacklist

--- lua/configs/lualine/lualine_b.lua
+++ lua/configs/lualine/lualine_b.lua
@@ -1,12 +1,33 @@
+local buffer_blacklist = {"neo-tree filesystem [1]"}
+
 ---@diagnostic disable-next-line: lowercase-global
-function notInNeotree()
-    local neotree_buffer = "neo-tree filesystem [1]"
-    local current_buffer = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
-    return neotree_buffer ~= current_buffer and vim.fn.mode() ~= "t"
+function DisplayFilename()
+  -- INFO: Check if we are currently in a terminal
+  if vim.fn.mode() == "t" then
+    return false
+  end
+
+  local current_buffer = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+
+  -- INFO: Don't need to check all of our blacklisted filename, its empty
+  if current_buffer == "" then
+    return false
+  end
+
+  local display = true
+  for _, str in ipairs(buffer_blacklist) do
+    if current_buffer == str then
+      -- INFO: Current buffer is in the blacklist, no need to search futher
+      display = false
+      break
+    end
+  end
+
+  return display
 end
 
 return {
     "branch",
-    { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }, cond = notInNeotree },
-    { "filename", path = 0, symbols = { modified = "󱇧", readonly = "󰷊", unnamed = "" }, cond = notInNeotree }
+    { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }, cond = DisplayFilename },
+    { "filename", path = 0, symbols = { modified = "󱇧", readonly = "󰷊", unnamed = "" }, cond = DisplayFilename }
 }
