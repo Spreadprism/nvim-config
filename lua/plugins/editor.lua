641202c295cd50cfd4d274f61b8ae61851471b45
Emeric Duguay <emeric.duguay1@bell.ca>
2023-10-24 14:58:29 -0400

    Modifed telescope configs
    - Added fzf extension
    - Added strict node_modules ignore

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -132,11 +132,36 @@ return {
 				},
 				pickers = {
 					find_files = {
-						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--glob",
+							"!**/.git/*",
+							"--glob",
+							"!**/node_modules/*",
+						},
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
 					},
 				},
 			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		dependencies = "nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		config = function()
+			require("telescope").load_extension("fzf")
 		end,
 	},
 	{
