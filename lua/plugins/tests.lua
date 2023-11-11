c5d13960913c51804e184ea2388f93955a10296b
Emeric Duguay <emericduguay@gmail.com>
2023-11-10 19:19:35 -0500

    Added python unit-testing

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -1,34 +1,42 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-neotest/neotest-go",
-      'haydenmeade/neotest-jest'
-    },
-    config = function ()
-      local neotest_namespace = vim.api.nvim_create_namespace("neotest")
-      require("neotest").setup({
-        adapters = {
-          require("neotest-go")({
-            experimental = {
-              test_table = true
-            },
-            args = { "-count=1", "-timeout=60s" }
-          }),
-          require('neotest-jest')({
-            jestCommand = "npm test --",
-            jestConfigFile = "custom.jest.config.ts",
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          })
-        }
-      })
-    end
-  },
+	{
+		"nvim-neotest/neotest",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-neotest/neotest-go",
+			"haydenmeade/neotest-jest",
+			"nvim-neotest/neotest-python",
+		},
+		config = function()
+			local neotest_namespace = vim.api.nvim_create_namespace("neotest")
+			require("neotest").setup({
+				adapters = {
+					-- INFO: Golang
+					require("neotest-go")({
+						experimental = {
+							test_table = true,
+						},
+						args = { "-count=1", "-timeout=60s" },
+					}),
+					-- INFO: Javascript
+					require("neotest-jest")({
+						jestCommand = "npm test --",
+						jestConfigFile = "custom.jest.config.ts",
+						env = { CI = true },
+						cwd = function(path)
+							return vim.fn.getcwd()
+						end,
+					}),
+					-- INFO: Python
+					require("neotest-python")({
+						python = require("utility.python_env_manager").get_python_path(),
+						dap = { justMyCode = true },
+					}),
+				},
+			})
+		end,
+	},
 }
