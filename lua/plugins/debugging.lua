2d7b2758a86dcdc8348940270402723fdaf7c54a
Emeric Duguay <emericduguay@gmail.com>
2023-11-14 21:29:16 -0500

    added repl auto-cmp

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -17,6 +17,21 @@ return {
 		end,
 	},
 	{
+		"rcarriga/cmp-dap",
+		config = function()
+			require("cmp").setup({
+				enabled = function()
+					return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+				end,
+			})
+			require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+				sources = {
+					{ name = "dap" },
+				},
+			})
+		end,
+	},
+	{
 		"mfussenegger/nvim-jdtls",
 		ft = "java",
 		dependencies = "mfussenegger/nvim-dap",
