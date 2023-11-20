2c846425d1a39013f5ae5214503ff00f21c47b9e
Emeric Duguay <emeric.duguay1@bell.ca>
2023-11-20 09:16:22 -0500

    added keybinds

--- lua/keys/lsp.lua
+++ lua/keys/lsp.lua
@@ -1,8 +1,9 @@
 local M = {}
 
 M.n = {
-  ["<leader>li"] = {":LspInfo<CR>", "Open lsp info", {silent = true}},
-  ["<leader>lr"] = {":LspRestart<CR>", "Restart lsp", {silent = true}},
+	["<leader>li"] = { ":LspInfo<CR>", "Open lsp info", { silent = true } },
+	["<leader>lr"] = { ":LspRestart<CR>", "Restart lsp", { silent = true } },
+	["<leader>ls"] = { ":LspStop<CR>", "Stop lsp", { silent = true } },
 }
 
 return M
