5b21a9ae9972ac81dbfca47d580aa559d82e0936
Emeric Duguay <emeric.duguay1@bell.ca>
2023-11-21 16:40:19 -0500

    Modified UI

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.showtabline = 0
 vim.o.updatetime = 300
 
 -- INFO: Init packages
--- lua/configs/lualine/lualine_y.lua
+++ lua/configs/lualine/lualine_y.lua
@@ -29,8 +29,27 @@ local get_os_symbol = function()
 	return os_symbol
 end
 
+local function separator()
+	return ""
+end
+
 return {
-	get_os_symbol,
+	{
+		get_os_symbol,
+	},
+	{
+		"tabs",
+		mode = 0,
+		separators = "",
+		cond = function()
+			return #vim.api.nvim_list_tabpages() > 1
+		end,
+		use_mode_colors = true,
+		-- tabs_color = {
+		-- 	inactive = { bg = "#3b4261" }, -- Color for inactive tab.
+		-- 	active = { fg = "#e4ac6c", bg = "#3b4261" }, -- Color for inactive tab.
+		-- },
+	},
 	-- function()
 	-- 	return require("copilot_status").status_string()
 	-- end,
