599e0c78aa337587e4405bece8b7f1ca0e9c97a3
Emeric Duguay <emericduguay@gmail.com>
2023-12-15 14:44:15 -0500

    Added harpoon marks indicator to lualine

--- lua/configs/lualine/lualine_b.lua
+++ lua/configs/lualine/lualine_b.lua
@@ -1,33 +1,64 @@
-local buffer_blacklist = {"neo-tree filesystem [1]"}
+local buffer_blacklist = { "neo-tree filesystem [1]" }
 
 ---@diagnostic disable-next-line: lowercase-global
 function DisplayFilename()
-  -- INFO: Check if we are currently in a terminal
-  if vim.fn.mode() == "t" then
-    return false
-  end
+	-- INFO: Check if we are currently in a terminal
+	if vim.fn.mode() == "t" then
+		return false
+	end
 
-  local current_buffer = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+	local current_buffer = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
 
-  -- INFO: Don't need to check all of our blacklisted filename, its empty
-  if current_buffer == "" then
-    return false
-  end
+	-- INFO: Don't need to check all of our blacklisted filename, its empty
+	if current_buffer == "" then
+		return false
+	end
 
-  local display = true
-  for _, str in ipairs(buffer_blacklist) do
-    if current_buffer == str then
-      -- INFO: Current buffer is in the blacklist, no need to search futher
-      display = false
-      break
-    end
-  end
+	local display = true
+	for _, str in ipairs(buffer_blacklist) do
+		if current_buffer == str then
+			-- INFO: Current buffer is in the blacklist, no need to search futher
+			display = false
+			break
+		end
+	end
 
-  return display
+	return display
+end
+
+local utils = require("harpoon.utils")
+local mark = require("harpoon.mark")
+
+function Displaymark()
+	if DisplayFilename() then
+		local current_buffer = utils.normalize_path(vim.api.nvim_buf_get_name(0))
+
+		for idx = 1, mark.get_length() do
+			if mark.get_marked_file_name(idx) == current_buffer then
+				return true
+			end
+		end
+	end
+	return false
 end
 
 return {
-    "branch",
-    { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }, cond = DisplayFilename },
-    { "filename", path = 0, symbols = { modified = "󱇧", readonly = "󰷊", unnamed = "" }, cond = DisplayFilename }
+	"branch",
+	{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }, cond = DisplayFilename },
+	{
+		"filename",
+		path = 0,
+		separator = "",
+		padding = { left = 1, right = 0 },
+		symbols = { modified = "󱇧", readonly = "󰷊", unnamed = "" },
+		cond = DisplayFilename,
+	},
+	{
+		function()
+			return "󰐃"
+		end,
+		color = { fg = "#db4b4b" },
+		separator = "",
+		cond = Displaymark,
+	},
 }
